9b7bc3ee23fb40494cc717eabc3c194ac22f4a20
David Afonso <david_r_afonso@hotmail.com>
2025-03-12 19:15:02 +0000

    Fix typo

--- Models/TestResult.cs
+++ Models/TestResult.cs
@@ -3,7 +3,7 @@
     public class TestResult
     {
         public int Id { get; set; }
-        public int TestSequenceId { get; set; } // Foreign key
+        public int TestSimulatorId { get; set; } // Foreign key
         public DateTime Timestamp { get; set; }
         public double RotationSpeed { get; set; } 
         public double StressLevel { get; set; } 
