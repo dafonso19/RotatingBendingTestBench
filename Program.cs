a51b30195c90c4f58326f8ea8b11f2483e91f1ab
David Afonso <david_r_afonso@hotmail.com>
2025-03-12 21:14:43 +0000

    Add test simulation service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using RotatingBendingTestBench.Data;
+using RotatingBendingTestBench.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
---
+++ Services/TestSimulationService.cs
@@ -0,0 +1,37 @@
+﻿using RotatingBendingTestBench.Models;
+
+
+namespace RotatingBendingTestBench.Services
+{
+    public class TestSimulationService
+    {
+        private readonly Random _random = new Random();
+
+        public List<TestResult> SimulateTest(TestSimulator simulator)
+        {
+            var results = new List<TestResult>();
+            var currentTime = DateTime.Now;
+
+            foreach (var data in simulator.Data)
+            {
+                for(var i = 0; i< data.Duration; i++)
+                {
+                    double speed = data.RotationSpeedSetpoint * (1 + _random.NextDouble() * 0.05 - 0.025); // ±2.5% variation
+                    double stress = speed * 0.1 + _random.NextDouble() * 10; //  (MPa)
+                    double temp = 20 + speed * 0.05 + _random.NextDouble() * 5; //  (Celsius)
+
+                    results.Add(new TestResult
+                    {
+                        TestSimulatorId = simulator.Id,
+                        Timestamp = currentTime.AddSeconds(temp),
+                        RotationSpeed = speed,
+                        StressLevel = stress,
+                        Temperature = temp
+                    });
+                }
+                currentTime = currentTime.AddSeconds(data.Duration);
+            }
+            return results;
+        }
+    }
+}
